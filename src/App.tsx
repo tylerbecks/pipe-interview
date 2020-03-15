3354ecc4242b79eecd6edcdc5c745e78a3c72eaf
Tyler Becks <tylerdbecks@gmail.com>
2020-03-15 15:22:36 -0700

    Add antd and tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Button } from "antd";
 import "./App.css";
 
 function App() {
@@ -7,17 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">Click Me!</Button>
+        <p>Hi, Zain!</p>
       </header>
     </div>
   );
